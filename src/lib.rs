d288bc4deb3b8f79a1bad06dcc057589f6585963
Linus Göldner <Linus@goeldner-family.de>
2024-02-11 23:47:24 +0100

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,50 @@ SJLL7
 |F--J
 LJ.LJ"];
 
+#[test]
+fn test1() {
+    let (pos, parsed) = parse(TEST_DATA[1]);
+    let mut curr_pos = Position::new(pos);
+	
+    loop {
+        curr_pos.walk(&parsed)
+    }
+}
+
+pub struct Part1;
+
+impl Part1 {
+    fn solution() {}
+}
+
+fn parse(input: &str) -> ((isize, isize), Vec<Vec<Pipe>>) {
+    let mut vec: Vec<Vec<Pipe>> = vec![];
+    let mut start_pos = None;
+
+    for (i, line) in input.lines().enumerate() {
+        let x = line
+            .chars()
+            .enumerate()
+            .map(|(j, ch)| {
+				dbg!(ch);
+                match ch {
+                    'S' => start_pos = Some((j as isize, i as isize)),
+                    _ => (),
+                }
+                Pipe::from(ch)
+            })
+            .collect();
+
+        vec.push(x)
+    }
+
+    let Some(start_pos) = start_pos else {
+        panic!("No starting position")
+    };
+
+    return (start_pos, vec);
+}
+
 #[derive(Debug, Clone, Copy)]
 pub struct Pipe(char);
 
@@ -29,7 +73,7 @@ pub struct Position {
 }
 
 impl Position {
-    pub fn walk(&mut self, map: Vec<Vec<Pipe>>) {
+    fn walk(&mut self, map: &Vec<Vec<Pipe>>) {
         // get the next pipe by calculating the offset
         let (dx, dy): (isize, isize) = match self.direction {
             Direction::U => (0, 1),
@@ -43,9 +87,22 @@ impl Position {
         self.x += dx;
         self.y += dy;
     }
+
+    fn new(coords: (isize, isize)) -> Self {
+        let (x, y) = coords;
+
+        Self {
+            x,
+            y,
+            direction: Direction::R,
+        }
+    }
 }
 
 impl Pipe {
+    fn from(ch: char) -> Self {
+        Self(ch)
+    }
     /// returns a function that takes a direction and returns a new direction
     pub fn to_direction(&self) -> impl Fn(Direction) -> Direction {
         match self.0 {
